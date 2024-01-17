945922bcd3e17910f6de6d99ce8ee977b818857f
encoderit-rafi <112605862+encoderit-rafi@users.noreply.github.com>
2024-01-17 18:39:08 +0600

    commit-3

--- src/assets/Icons/StarOutline.jsx
+++ src/assets/Icons/StarOutline.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const StarOutline = () => {
   return (
     <div className="w-24 text-orange-400">
--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,10 +1,13 @@
-import Star from "./Icons/Star";
-import StarOutline from "./Icons/StarOutline";
-const StarRating = ({}) => {
+import Star from "../assets/Icons/Star.jsx";
+import StarOutline from "../assets/Icons/StarOutline.jsx";
+const StarRating = ({ maxRating = 5 }) => {
   return (
     <div className="flex items-center justify-center space-x-4">
-      <Star />
-      <StarOutline />
+      {/* <Star /> */}
+      {/* <StarOutline /> */}
+      {Array.from({ length: maxRating }, (_, i) => (
+        <StarOutline key={i} />
+      ))}
     </div>
   );
 };
